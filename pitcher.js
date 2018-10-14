b0e06b05d61c118e3ffcbef9e408d0e1143bf79d
root <root@ip-172-31-20-21.ec2.internal>
2018-10-14 04:00:07 +0000

    Pass enable

--- pitcher.js
+++ pitcher.js
@@ -1,9 +1,8 @@
-const test;
 var rp = require('request-promise');
 
 var options = {
     method: 'POST',
-    uri: 'ec2-34-229-116-79.compute-1.amazonaws.com/catcher',
+    uri: 'http://ec2-18-212-105-175.compute-1.amazonaws.com/catcher',
     body: {
         some: 'payload'
     },
@@ -12,11 +11,11 @@ var options = {
 
 
 module.exports = {
-	passit: function{
+	passit: function(){
 		rp(options).then(function (parsedBody) {
-        	console.out("pass made");
+        	console.log("pass made");
     		}).catch(function (err) {
-        	console.out("Pass failed");
+        	console.log("Pass failed");
     		});
 	}
 };
